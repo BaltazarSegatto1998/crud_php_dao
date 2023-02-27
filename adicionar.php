d7a57f01a2fe44ba12b97ea80ed48476083c7e21
Sidnei Baltazar Segatto Junior <sidneibaltazar1998@gmail.com>
2023-02-27 17:09:01 -0300

    DAO

---
+++ adicionar.php
@@ -0,0 +1,14 @@
+<h1>Adicionar Usuário</h1>
+<form method="POST" action="adicionar_action.php">
+    <label for="name">
+        Nome:
+        <input id="name" type="text" name="name"/>
+    </label><br><br>
+
+    <label for="email">
+        E-mail:
+        <input id="name" type="email" name="email"/>
+    </label><br><br>
+
+    <input type="submit" value="Adicionar">
+</form>
